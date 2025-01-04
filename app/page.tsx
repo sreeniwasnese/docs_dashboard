3224513777ba756244149774b52c738cce957755
sreeniwas <sreeniwas.it@gmail.com>
2025-01-04 22:18:16 +0530

    basics on fonts and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 import AcmeLogo from '@/app/ui/acme-logo';
+import { lusitana } from './ui/fonts';
 import styles from "@/app/ui/home.module.css"
 
 
@@ -11,7 +13,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
 
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
 
 
@@ -21,7 +23,7 @@ export default function Page() {
           {/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" /> */}
           <div className={styles.shape}></div>
 
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -37,7 +39,21 @@ export default function Page() {
         </div>
 
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+
+          <Image 
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className='block md:hidden'
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
 
