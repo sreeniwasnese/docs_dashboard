42f1bd358c11e3a861863d409750a865ee4e5594
sreeniwasnese <sreeniwas.nese.it@gmail.com>
2025-01-05 13:49:50 +0530

    completed adding dynamic / stagered data fetching for rendering

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,17 +1,16 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+
+import { fetchRevenue } from '@/app/lib/data';
 
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
 
   const chartHeight = 350;
+  const revenue = await fetchRevenue()
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
+
 
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
